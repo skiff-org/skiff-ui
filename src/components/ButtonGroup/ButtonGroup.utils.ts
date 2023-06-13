a0b6c98141bc976b8a3b1871f0bba5be6d142480
Khadija Hammoud <khammoud@nightwatchapp.com>
2023-06-13 18:06:01 +0300

    Rm active prop

--- src/components/ButtonGroup/ButtonGroup.utils.ts
+++ src/components/ButtonGroup/ButtonGroup.utils.ts
@@ -1,7 +1,6 @@
 import { Type } from '../../types';
 
-export const getButtonType = (index: number, destructive?: boolean) => {
-  if (!!destructive) return Type.DESTRUCTIVE;
+export const getButtonType = (index: number) => {
   if (index === 0) return Type.PRIMARY;
   if (index === 2) return Type.TERTIARY;
   return Type.SECONDARY;
